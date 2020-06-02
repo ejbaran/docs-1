1d07987cf4330270fba649cdb5969f4452d12b6e
ryanrfox <ryan@ryanrfox.com>
2020-06-02 16:54:33 -0400

    Fixup -target path

--- scripts/reformat-all-commands.sh
+++ scripts/reformat-all-commands.sh
@@ -3,29 +3,27 @@
 # SETUP
 GO_ALGORAND_SRC=$1
 INDEXER_SRC=$2
-mkdir -p ../docs/reference-docs/rest-apis/algod/v1
-mkdir -p ../docs/reference-docs/rest-apis/algod/v2
 
 # CLI GOAL
-./reformat.py -doc-dir ../docs/reference-docs/goal/ -cmd ~/go/bin/goal
+./reformat.py -doc-dir ../docs/reference/cli/goal/ -cmd ~/go/bin/goal
 
 # CLI ALGOKEY
-./reformat.py -doc-dir ../docs/reference-docs/algokey/ -cmd ~/go/bin/algokey
+./reformat.py -doc-dir ../docs/reference/cli/algokey/ -cmd ~/go/bin/algokey
 
 # CLI KMD
-./reformat.py -doc-dir ../docs/reference-docs/kmd/ -cmd ~/go/bin/kmd
+./reformat.py -doc-dir ../docs/reference/cli/kmd/ -cmd ~/go/bin/kmd
 
 # REST KMD
-./convert_swagger.py -target ../docs/reference-docs/rest-apis/kmd.md -specfile $GO_ALGORAND_SRC/daemon/kmd/api/swagger.json
+./convert_swagger.py -target ../docs/reference/rest-apis/kmd.md -specfile $GO_ALGORAND_SRC/daemon/kmd/api/swagger.json
 
 # REST ALGOD V1
-./convert_swagger.py -target ../docs/reference-docs/rest-apis/algod/v1/algod.md -specfile $GO_ALGORAND_SRC/daemon/algod/api/swagger.json -processors algod_specfile_processors
+./convert_swagger.py -target ../docs/reference/rest-apis/algod/v1.md -specfile $GO_ALGORAND_SRC/daemon/algod/api/swagger.json -processors algod_specfile_processors
 
 # REST ALGOD V2 (from algod.oas2.json)
-./convert_swagger.py -target ../docs/reference-docs/rest-apis/algod/v2/algod.md -specfile $GO_ALGORAND_SRC/daemon/algod/api/algod.oas2.json
+./convert_swagger.py -target ../docs/reference/rest-apis/algod/v2.md -specfile $GO_ALGORAND_SRC/daemon/algod/api/algod.oas2.json
 
 # REST INDEXER
-./convert_swagger.py -target ../docs/reference-docs/rest-apis/indexer.md -specfile $INDEXER_SRC/api/indexer.oas2.json 
+./convert_swagger.py -target ../docs/reference/rest-apis/indexer.md -specfile $INDEXER_SRC/api/indexer.oas2.json 
 
 # CLEANUP
 rm swagger2markup-cli.jar
